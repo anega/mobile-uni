8a5bf932a410f212a4ed2788a3bec97f2e5b9ead
anega <anegamega@gmail.com>
2023-11-18 20:41:47 +0200

    Lab2: small refactorings(redundant ; and organize imports)

--- lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -23,13 +23,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -42,9 +40,9 @@ import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
-    private lateinit var drawerState: DrawerState;
-    private lateinit var scope: CoroutineScope;
-    private lateinit var navController: NavHostController;
+    private lateinit var drawerState: DrawerState
+    private lateinit var scope: CoroutineScope
+    private lateinit var navController: NavHostController
 
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,8 +50,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab2Theme {
                 drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-                scope = rememberCoroutineScope();
-                navController = rememberNavController();
+                scope = rememberCoroutineScope()
+                navController = rememberNavController()
                 val currentNavBackStackEntry by navController.currentBackStackEntryAsState()
                 val currentRoute = currentNavBackStackEntry?.destination?.route ?: "home"
                 // A surface container using the 'background' color from the theme
