1b10168fbdfc2ec6af86c9ed1623abfc4e3df771
anega <anegamega@gmail.com>
2023-11-18 14:09:43 +0200

    Lab2: add HomeScreen starter template

---
+++ lab2/app/src/main/java/com/example/lab2/screens/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.lab2.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Column {
+        Text(text = "Home screen")
+    }
+}
