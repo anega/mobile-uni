07d6ad4f09e4bd3d9416a1485a9c1c687f36b3f8
anega <anegamega@gmail.com>
2023-11-20 18:55:07 +0200

    Lab2: add icon to the dravable Home item

--- lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -26,6 +27,8 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -64,7 +67,16 @@ class MainActivity : ComponentActivity() {
                         drawerContent = {
                             ModalDrawerSheet {
                                 NavigationDrawerItem(
-                                    label = { Text(text = "Home") },
+                                    label = {
+                                        Row {
+                                            Icon(
+                                                painter = painterResource(id = R.drawable.ic_photo_camera),
+                                                contentDescription = "Home screen",
+                                                modifier = Modifier.padding(end = 12.dp)
+                                            )
+                                            Text(text = "Home")
+                                        }
+                                    },
                                     selected = currentRoute == "home",
                                     onClick = {
                                         navController.navigate("home") {
@@ -83,7 +95,7 @@ class MainActivity : ComponentActivity() {
                                         scope.launch { drawerState.close() }
                                     })
                                 NavigationDrawerItem(
-                                    label = { Text(text = "Slideshow")},
+                                    label = { Text(text = "Slideshow") },
                                     selected = currentRoute == "slideshow",
                                     onClick = {
                                         navController.navigate("slideshow") {
