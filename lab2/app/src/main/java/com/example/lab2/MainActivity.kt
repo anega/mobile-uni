604c3b7f0a75156bd64a3acd005b25eb934284e8
anega <anegamega@gmail.com>
2023-11-20 20:30:30 +0200

    Lab2: add navigation drawer header

--- lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.lab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerState
@@ -27,9 +32,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -67,63 +77,106 @@ class MainActivity : ComponentActivity() {
                         drawerState = drawerState,
                         drawerContent = {
                             ModalDrawerSheet {
-                                NavigationDrawerItem(
-                                    label = {
-                                        Row {
-                                            Icon(
-                                                painter = painterResource(id = R.drawable.ic_photo_camera),
-                                                contentDescription = "Home screen",
-                                                modifier = Modifier.padding(end = 12.dp)
-                                            )
-                                            Text(text = "Home")
-                                        }
-                                    },
-                                    selected = currentRoute == "home",
-                                    onClick = {
-                                        navController.navigate("home") {
-                                            popUpTo("home")
-                                        }
-                                        scope.launch { drawerState.close() }
-                                    })
-                                NavigationDrawerItem(
-                                    label = {
-                                        Row {
-                                            Icon(
-                                                painter = painterResource(id = R.drawable.ic_menu_gallery),
-                                                contentDescription = "Gallery screen icon",
-                                                modifier = Modifier.padding(end = 12.dp)
-                                            )
-                                            Text(text = "Gallery")
-                                        }
-                                    },
-                                    selected = currentRoute == "gallery",
-                                    onClick = {
-                                        navController.navigate("gallery") {
-                                            launchSingleTop = true
-                                            restoreState = true
-                                        }
-                                        scope.launch { drawerState.close() }
-                                    })
-                                NavigationDrawerItem(
-                                    label = {
-                                        Row {
-                                            Icon(
-                                                painter = painterResource(id = R.drawable.ic_menu_gallery),
-                                                contentDescription = "Gallery screen icon",
-                                                modifier = Modifier.padding(end = 12.dp)
-                                            )
-                                            Text(text = "Slideshow")
-                                        }
-                                    },
-                                    selected = currentRoute == "slideshow",
-                                    onClick = {
-                                        navController.navigate("slideshow") {
-                                            launchSingleTop = true
-                                            restoreState = true
-                                        }
-                                        scope.launch { drawerState.close() }
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(bottom = 8.dp)
+                                        .background(MaterialTheme.colorScheme.primaryContainer)
+                                        .padding(20.dp)
+                                ) {
+                                    Box(
+                                        contentAlignment = Alignment.Center,
+                                        modifier = Modifier
+                                            .padding(bottom = 8.dp)
+                                            .size(80.dp)
+                                            .clip(CircleShape)
+                                    ) {
+                                        Image(
+                                            painter = painterResource(R.drawable.ic_launcher_background),
+                                            contentDescription = "Android icon background",
+                                            contentScale = ContentScale.FillBounds,
+                                            modifier = Modifier.fillMaxSize()
+                                        )
+                                        Icon(
+                                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                                            contentDescription = "Android icon",
+                                            tint = Color.White,
+                                        )
                                     }
-                                )
+                                    Text(
+                                        text = "Android Studio",
+                                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                                    )
+                                    Text(
+                                        text = "android.studio@android.com",
+                                        fontSize = 12.sp,
+                                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                                    )
+                                }
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(horizontal = 4.dp)
+                                ) {
+                                    NavigationDrawerItem(
+                                        modifier = Modifier.padding(horizontal = 4.dp),
+                                        label = {
+                                            Row {
+                                                Icon(
+                                                    painter = painterResource(id = R.drawable.ic_photo_camera),
+                                                    contentDescription = "Home screen",
+                                                    modifier = Modifier.padding(end = 12.dp)
+                                                )
+                                                Text(text = "Home")
+                                            }
+                                        },
+                                        selected = currentRoute == "home",
+                                        onClick = {
+                                            navController.navigate("home") {
+                                                popUpTo("home")
+                                            }
+                                            scope.launch { drawerState.close() }
+                                        })
+                                    NavigationDrawerItem(
+                                        label = {
+                                            Row {
+                                                Icon(
+                                                    painter = painterResource(id = R.drawable.ic_menu_gallery),
+                                                    contentDescription = "Gallery screen icon",
+                                                    modifier = Modifier.padding(end = 12.dp)
+                                                )
+                                                Text(text = "Gallery")
+                                            }
+                                        },
+                                        selected = currentRoute == "gallery",
+                                        onClick = {
+                                            navController.navigate("gallery") {
+                                                launchSingleTop = true
+                                                restoreState = true
+                                            }
+                                            scope.launch { drawerState.close() }
+                                        })
+                                    NavigationDrawerItem(
+                                        label = {
+                                            Row {
+                                                Icon(
+                                                    painter = painterResource(id = R.drawable.ic_menu_gallery),
+                                                    contentDescription = "Gallery screen icon",
+                                                    modifier = Modifier.padding(end = 12.dp)
+                                                )
+                                                Text(text = "Slideshow")
+                                            }
+                                        },
+                                        selected = currentRoute == "slideshow",
+                                        onClick = {
+                                            navController.navigate("slideshow") {
+                                                launchSingleTop = true
+                                                restoreState = true
+                                            }
+                                            scope.launch { drawerState.close() }
+                                        }
+                                    )
+                                }
                             }
                         }) {
                         Scaffold(
