55804d4613e0502dd7d1a4cfb1b079d4ae4484f5
anega <anegamega@gmail.com>
2023-11-21 10:54:36 +0200

    Lab2: refactor appbar composable into separate file

--- lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -13,13 +13,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
@@ -27,8 +24,6 @@ import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
@@ -45,6 +40,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.lab2.components.ApplicationTopBar
 import com.example.lab2.screens.GalleryScreen
 import com.example.lab2.screens.HomeScreen
 import com.example.lab2.screens.SlideshowScreen
@@ -181,23 +177,10 @@ class MainActivity : ComponentActivity() {
                         }) {
                         Scaffold(
                             topBar = {
-                                TopAppBar(
-                                    title = { Text(text = currentRoute.replaceFirstChar { it.uppercase() }) },
-                                    modifier = Modifier.fillMaxWidth(),
-                                    colors = TopAppBarDefaults.smallTopAppBarColors(
-                                        containerColor = MaterialTheme.colorScheme.primaryContainer
-                                    ),
-                                    navigationIcon = {
-                                        IconButton(
-                                            onClick = { scope.launch { drawerState.open() } },
-                                            content = {
-                                                Icon(
-                                                    imageVector = Icons.Default.Menu,
-                                                    contentDescription = null
-                                                )
-                                            }
-                                        )
-                                    }
+                                ApplicationTopBar(
+                                    currentRoute = currentRoute,
+                                    drawerState = drawerState,
+                                    scope = scope
                                 )
                             }
                         ) {
