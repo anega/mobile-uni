62dbb6a235f7ba9fa7c5e8334ed70d3bc1a7dc36
anega <anegamega@gmail.com>
2023-11-21 17:19:39 +0200

    Lab2: add button to go from tab activity to main activity

--- lab2/app/src/main/java/com/example/lab2/screens/tabs/MyCustomTabScreen.kt
+++ lab2/app/src/main/java/com/example/lab2/screens/tabs/MyCustomTabScreen.kt
@@ -1,20 +1,36 @@
 package com.example.lab2.screens.tabs
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.lab2.MainActivity
 
 @Composable
 fun MyCustomTabScreen() {
+    val context = LocalContext.current
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
         Text(text = "My custom tab screen")
+        Button(
+            modifier = Modifier.padding(16.dp),
+            onClick = {
+                val intent = Intent(context, MainActivity::class.java)
+                context.startActivity(intent)
+            }
+        ) {
+            Text(text = "Go to the MainActivity")
+        }
     }
 }
--- lab2/app/src/main/java/com/example/lab2/screens/tabs/PostScreen.kt
+++ lab2/app/src/main/java/com/example/lab2/screens/tabs/PostScreen.kt
@@ -3,10 +3,12 @@ package com.example.lab2.screens.tabs
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PostScreen() {
@@ -16,5 +18,9 @@ fun PostScreen() {
         modifier = Modifier.fillMaxSize()
     ) {
         Text(text = "Post screen")
+        Text(
+            text = "Кнопка, що переведе вас до першої активності знаходиться на табі 'MuCustomTab'",
+            modifier = Modifier.padding(20.dp, 12.dp, 20.dp, 0.dp)
+        )
     }
 }
