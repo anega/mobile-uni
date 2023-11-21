ddff9317e02f1fcd552136fc5ba2a4c1df194c5f
anega <anegamega@gmail.com>
2023-11-21 11:26:59 +0200

    Lab2: add my custom screen composable

--- lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -3,32 +3,8 @@ package com.example.lab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DrawerState
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.example.lab2.components.ApplicationDrawer
-import com.example.lab2.components.ApplicationTopBar
-import com.example.lab2.screens.GalleryScreen
-import com.example.lab2.screens.HomeScreen
 import com.example.lab2.screens.MainAppScreen
-import com.example.lab2.screens.SlideshowScreen
 import com.example.lab2.ui.theme.Lab2Theme
-import kotlinx.coroutines.CoroutineScope
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
