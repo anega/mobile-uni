6a0ec30524a1f62a70ac4c0fefab8e27aee99d68
anega <anegamega@gmail.com>
2023-11-23 17:41:45 +0200

    Lab3: change paddings for the LazyCol and spasing between items

--- lab3/app/src/main/java/com/example/lab3/MainActivity.kt
+++ lab3/app/src/main/java/com/example/lab3/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.lab3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.lab3.components.MovieListItem
 import com.example.lab3.models.MovieRepository
 import com.example.lab3.ui.theme.Lab3Theme
@@ -26,7 +29,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LazyColumn {
+                    LazyColumn(
+                        contentPadding = PaddingValues(all = 12.dp),
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
                         items(items = movies) { movie ->
                             MovieListItem(item = movie)
                         }
