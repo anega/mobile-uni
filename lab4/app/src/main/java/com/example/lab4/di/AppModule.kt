a3c5a797afa1a9bca69ae5038c2a68a325753688
euulu <anegamega@gmail.com>
2023-12-01 16:18:16 +0200

    Lab4: add hilt module for db and repository

---
+++ lab4/app/src/main/java/com/example/lab4/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.example.lab4.di
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.example.lab4.data.MoviesDatabase
+import com.example.lab4.data.MoviesRepository
+import com.example.lab4.data.MoviesRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun providesMoviesDatabase(app: Application): MoviesDatabase {
+        return Room
+            .databaseBuilder(app, MoviesDatabase::class.java, "movies_db")
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesMoviesRepository(db: MoviesDatabase): MoviesRepository {
+        return MoviesRepositoryImpl(db.dao)
+    }
+}
