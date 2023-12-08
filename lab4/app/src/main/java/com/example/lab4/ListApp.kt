540590b34184948a0b268cc8724a2c96a97b7b6f
euulu <anegamega@gmail.com>
2023-12-08 15:16:21 +0200

    Lab4: setup hilt injection for the app

---
+++ lab4/app/src/main/java/com/example/lab4/ListApp.kt
@@ -0,0 +1,8 @@
+package com.example.lab4
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ListApp : Application() {
+}
--- lab4/app/src/main/java/com/example/lab4/MainActivity.kt
+++ lab4/app/src/main/java/com/example/lab4/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab4.ui.theme.Lab4Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,26 +23,8 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ) {}
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab4Theme {
-        Greeting("Android")
     }
 }
