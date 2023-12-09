75153c7e701eca192f8c89b68eff97d1d9a0bd21
euulu <anegamega@gmail.com>
2023-12-09 21:10:20 +0200

    Lab4: use ListScreen in main activity

--- lab4/app/src/main/java/com/example/lab4/MainActivity.kt
+++ lab4/app/src/main/java/com/example/lab4/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab4.ui.list.ListScreen
 import com.example.lab4.ui.theme.Lab4Theme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,7 +21,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {}
+                ) {
+                    ListScreen()
+                }
             }
         }
     }
