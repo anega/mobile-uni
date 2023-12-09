697c9af87c8986281842bf9c54e1738ca25c9690
euulu <anegamega@gmail.com>
2023-12-09 21:08:50 +0200

    Lab4: inject viewmodel into ListScreen and get movies list

--- lab4/app/src/main/java/com/example/lab4/ui/list/ListScreen.kt
+++ lab4/app/src/main/java/com/example/lab4/ui/list/ListScreen.kt
@@ -5,18 +5,21 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.unit.dp
-import com.example.lab4.data.models.Movie
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun ListScreen(
-    data: List<Movie>
+    viewModel: ListViewModel = hiltViewModel()
 ) {
+    val movies = viewModel.movies.collectAsState(initial = emptyList())
+
     LazyColumn(
         contentPadding = PaddingValues(all = 12.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        items(items = data) { item ->
+        items(items = movies.value) { item ->
             MovieListItem(item = item)
         }
     }
