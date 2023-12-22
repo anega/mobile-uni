46755e08869309fa865d74ae1de1512864446fcf
euulu <anegamega@gmail.com>
2023-12-22 14:44:15 +0200

    Lab4: add FAB to add movie to the List of movies

--- lab4/app/src/main/java/com/example/lab4/ui/list/ListScreen.kt
+++ lab4/app/src/main/java/com/example/lab4/ui/list/ListScreen.kt
@@ -1,26 +1,47 @@
 package com.example.lab4.ui.list
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
 
 @Composable
 fun ListScreen(
-    viewModel: ListViewModel = hiltViewModel()
+    viewModel: ListViewModel = hiltViewModel(),
+    navController: NavHostController
 ) {
     val movies = viewModel.movies.collectAsState(initial = emptyList())
 
-    LazyColumn(
-        contentPadding = PaddingValues(all = 12.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        items(items = movies.value) { item ->
-            MovieListItem(item = item)
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate(route = "movie/-1")
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add button"
+                )
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            contentPadding = paddingValues,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(items = movies.value) { item ->
+                MovieListItem(item = item, navController = navController)
+            }
         }
     }
 }
