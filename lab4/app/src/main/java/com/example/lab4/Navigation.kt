9546b1e0876f9ab07985b8da56f863f870a881f4
euulu <anegamega@gmail.com>
2023-12-22 14:41:20 +0200

    Lab4: add Navigation

---
+++ lab4/app/src/main/java/com/example/lab4/Navigation.kt
@@ -0,0 +1,34 @@
+package com.example.lab4
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.lab4.ui.list.ListScreen
+import com.example.lab4.ui.movie.MovieScreen
+
+@Composable
+fun Navigation(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "list"
+    ) {
+        composable(
+            route = "list"
+        ) {
+            ListScreen(navController = navController)
+        }
+        composable(
+            route = "movie/{id}",
+            arguments = listOf(navArgument("id") {
+                type = NavType.IntType
+            })
+        ) {
+            MovieScreen(navController = navController)
+        }
+    }
+}
