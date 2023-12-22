97d2f81ff8189f7dbd1ddf0d6e7435e47144d907
euulu <anegamega@gmail.com>
2023-12-22 14:42:03 +0200

    Lab4: use navigation composable in the MainActivity

--- lab4/app/src/main/java/com/example/lab4/MainActivity.kt
+++ lab4/app/src/main/java/com/example/lab4/MainActivity.kt
@@ -7,22 +7,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.lab4.ui.list.ListScreen
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.lab4.ui.theme.Lab4Theme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Lab4Theme {
-                // A surface container using the 'background' color from the theme
+                navController = rememberNavController()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ListScreen()
+                    Navigation(navController = navController)
                 }
             }
         }
