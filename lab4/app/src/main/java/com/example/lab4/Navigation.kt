6a8f0b277e0eeb5452dd79eda4dde5e504fdd57b
euulu <anegamega@gmail.com>
2023-12-26 16:50:12 +0200

    Lab4: update movie id on route change(when navigate to single movie screen)

--- lab4/app/src/main/java/com/example/lab4/Navigation.kt
+++ lab4/app/src/main/java/com/example/lab4/Navigation.kt
@@ -1,17 +1,22 @@
 package com.example.lab4
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.example.lab4.ui.list.ListScreen
+import com.example.lab4.ui.movie.AddEditMovieEvent
 import com.example.lab4.ui.movie.MovieScreen
+import com.example.lab4.ui.movie.MovieViewModel
 
 @Composable
 fun Navigation(
-    navController: NavHostController
+    navController: NavHostController,
+    viewModel: MovieViewModel = hiltViewModel()
 ) {
     NavHost(
         navController = navController,
@@ -27,8 +32,12 @@ fun Navigation(
             arguments = listOf(navArgument("id") {
                 type = NavType.IntType
             })
-        ) {
-            MovieScreen()
+        ) { navBackStackEntry ->
+            val currentMovieId = navBackStackEntry.arguments!!.getInt("id")
+            LaunchedEffect(key1 = currentMovieId) {
+                viewModel.onEvent(AddEditMovieEvent.OnIdChange(currentMovieId))
+            }
+            MovieScreen(navController = navController)
         }
     }
 }
