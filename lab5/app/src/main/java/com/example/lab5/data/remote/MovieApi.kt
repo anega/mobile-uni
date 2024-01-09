15e406408d38a4d26db48ff2fd95685df1d93692
euulu <anegamega@gmail.com>
2024-01-09 21:15:48 +0200

    Lab5: add MovieApi service

---
+++ lab5/app/src/main/java/com/example/lab5/data/remote/MovieApi.kt
@@ -0,0 +1,16 @@
+package com.example.lab5.data.remote
+
+import com.example.lab5.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApi {
+    @GET("movie/popular?api_key=${BuildConfig.TMDB_APIKEY}&language=en-US")
+    suspend fun getMovies(
+        @Query("page") page: Int
+    ): ResponseDto
+
+    companion object {
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+    }
+}
