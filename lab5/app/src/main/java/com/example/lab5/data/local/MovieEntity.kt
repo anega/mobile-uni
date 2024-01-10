76e7c7470b87875c27e3a6d8f462c25505af316a
euulu <anegamega@gmail.com>
2024-01-10 13:11:49 +0200

    Lab5: add mapper function from MovieEntity to Movie

--- lab5/app/src/main/java/com/example/lab5/data/local/MovieEntity.kt
+++ lab5/app/src/main/java/com/example/lab5/data/local/MovieEntity.kt
@@ -3,6 +3,7 @@ package com.example.lab5.data.local
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.lab5.domain.model.Movie
 
 @Entity(tableName = "movies")
 data class MovieEntity(
@@ -15,6 +16,17 @@ data class MovieEntity(
     @ColumnInfo("release_date")
     val releaseDate: String,
     @ColumnInfo("vote_average")
-    val voteAverage: String,
+    val voteAverage: Double,
     val page: Int
-)
+) {
+    fun toMovie(): Movie {
+        return Movie(
+            id = id,
+            title = title,
+            posterPath = posterPath,
+            overview = overview,
+            releaseDate = releaseDate,
+            voteAverage = voteAverage
+        )
+    }
+}
